615c4354afb0782fc135ddbef9bc1032c550f45a
Nicholas Otto <nwkotto@umich.edu>
2016-11-14 14:53:39 -0500

    Added support for multiple environments and uglifying the source for distribution

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Hero App Business Portal
 
+## Config
+
+Before running or building, you need to add a .env file to the root of the project (we use dotenv - https://github.com/motdotla/dotenv).  Your .env should look something like this:
+
+```
+	PROD_API_DOMAIN='https://example.com'
+	STAGE_API_DOMAIN='https://stage.example.com'
+```
+If, for whatever reason, you want to build the app with a different api domain, just add <ENV>_API_DOMAIN to the .env and run 'webpack --env=<ENV>'.
+
 ## Running locally
 
 ```
 	// In the root directory
 	npm install
-	npm start // By default, starts a live reloading dev server at 
+	npm start // By default, starts a live reloading dev server at http://localhost:8080/webpack-dev-server/
 ```
 
 ## Deploying
@@ -15,4 +25,4 @@
 	npm run build
 ```
 
-After running the above command, the webpack bundle will be in the dist directory.  Copy bundle.js and index.html to your static site host, making sure both files are in the root directory.
+This will put the production-ready webpack bundle in the dist directory.  Copy bundle.min.js and index.html to your static site host, making sure both files are in the root directory.
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,45 @@
+require('dotenv').config()
+var webpack = require('webpack');
+
+// Process args
+var context = (function() {
+    var suffixes = ['API_DOMAIN'],
+        env = process.argv.filter(function(val) { return val.indexOf('--env') > -1 });
+    if (env.length === 0) {
+        throw 'Error: Must provide a --env environment';
+    }
+    env = env[0].split('=')[1];
+    return suffixes.reduce(function(args, suffix) {
+        var envVar = process.env[`${env}_${suffix}`];
+        if (!envVar) {
+            throw `Error: Must specify ${env}_${suffix} in the .env file in the root`
+        }
+        args[`${suffix}`] = envVar;
+        return args;
+    }, {
+        'ENV': env
+    });
+})()
+console.log(context);
+
 module.exports = {
     context: __dirname + '/src',
     entry: './index.js',
     output: {
         path: __dirname + '/dist',
-        filename: 'bundle.js'
+        filename: context.ENV === 'PROD' ? 'bundle.min.js' : 'bundle.js'
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'API_DOMAIN': JSON.stringify(context.API_DOMAIN)
+            }
+        })
+    ].concat(context.ENV === 'PROD' ? [
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false }
+        })
+    ] : []),
     module: {
         loaders: [
             {
