2b38117d12402d524b398f99ba5bb8d47a092d72
smoliver <samuelolive21@gmail.com>
2016-11-27 19:24:41 -0500

    Business logic added and passed down to components of router

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,31 @@ import Dashboard from './components/dashboard/Dashboard';
 import LogIn from './components/auth/LogIn';
 import SignUp from './components/auth/SignUp';
 
-const Routes = () => (
-    <Router history={hashHistory}>
-        <Route path="/" component={Main}>
-            <Route path="dashboard" component={Dashboard} />
-            <Route path="signup" component={SignUp} />
-            <Route path="login" component={LogIn} />
-        </Route>
-    </Router>
-)
+class Routes extends React.Component {
+    constructor() {
+        super();
 
-export default Routes;
+        this.createElement = this.createElement.bind(this);
+    }
+
+
+    createElement(Component, props) {
+        console.log("Routes Business");
+        console.log(this.props.business);
+        return <Component {...props} business={this.props.business} onUpdateBusiness={this.props.onUpdateBusiness} />
+    }
+
+    render() {
+        return(
+            <Router history={hashHistory} createElement={this.createElement}>
+                <Route path="/" component={Main}>
+                    <Route path="dashboard" component={Dashboard} />
+                    <Route path="signup" component={SignUp} />
+                    <Route path="login" component={LogIn} />
+                </Route>
+            </Router>
+        )
+    }
+}
+
+export default Routes
